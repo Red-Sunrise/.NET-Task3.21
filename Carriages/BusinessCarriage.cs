99e4c8821d7f9147cddb4d61cf5046b80f655503
Red-Sunrise <yaroslaw_proskurin@mail.ru>
2021-10-19 02:15:21 +0300

    Вагоны

---
+++ Carriages/BusinessCarriage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class BusinessCarriage : Carriage
+    {
+        public BusinessCarriage(string title,int comfortLevel, int passengerAmount, int baggageAmount) : base(comfortLevel, passengerAmount, baggageAmount)
+        {
+            Title = "Бизнес " + base.Title + title;
+        }
+        public BusinessCarriage()
+        {
+            Title = "Бизнес " + base.Title;
+            ComfortLevel = 10;
+            PassengerAmount = 5;
+            BaggageAmount = 10;
+        }
+    }
+}
---
+++ Carriages/EconomCarriage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class EconomCarriage : Carriage
+    {
+        public EconomCarriage(string title,int comfortLevel, int passengerAmount, int baggageAmount) : base(comfortLevel, passengerAmount, baggageAmount)
+        {
+            Title = "Эконом " + base.Title + title;
+        }
+        public EconomCarriage()
+        {
+            Title = "Эконом " + base.Title;
+            ComfortLevel = 3;
+            PassengerAmount = 30;
+            BaggageAmount = 20;
+        }
+    }
+}
