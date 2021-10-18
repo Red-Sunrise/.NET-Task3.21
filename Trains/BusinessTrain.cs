896bd5bf17cba8f1f548064631140596ec985a73
Red-Sunrise <yaroslaw_proskurin@mail.ru>
2021-10-19 02:16:27 +0300

    Поезда

---
+++ Trains/BusinessTrain.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class BusinessTrain : Train
+    {
+        public BusinessTrain() : base()
+        {
+            Title = "Бизнес Поезд : ";
+            AddCarriage(5, new BusinessCarriage());
+        }
+    }
+}
---
+++ Trains/EconomTrain.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class EconomTrain : Train
+    {
+        public EconomTrain() : base()
+        {
+            Title = "Эконом Поезд :";
+            AddCarriage(5, new EconomCarriage());
+        }
+    }
+}
